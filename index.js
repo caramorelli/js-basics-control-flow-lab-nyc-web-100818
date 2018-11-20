443f7a1997be04fe04d183bccb5b49904088f7e7
Cara Morelli <caramorelli7@gmail.com>
2018-11-20 15:34:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function scuberGreetingForFeet(num) {
         return 'Thank you so much.'
       case 'not as generous':
         return 'Thank you.'
-      else:
+      case:
         return 'Bye'
     }
   }
